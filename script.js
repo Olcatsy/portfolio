ea3de88b20ac366fb938758b34a4c574c49992aa
Olga <olga.filiushkina@gmail.com>
2020-04-14 00:21:08 -0400

    the mobile menu takes up the whole window now

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $(function() {
-    $('.hamburger').on('click', () => {
+    $('#hamburger').on('click', () => {
       $('.nav ul').slideDown();
     })
 
-    $('.close').on('click', () => {
+    $('#close').on('click', () => {
+      $('.nav ul').slideUp();
+    })
+
+    $('.nav-link').on('click', () => {
       $('.nav ul').slideUp();
     })
     
