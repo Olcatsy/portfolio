d4eed5f5eca453df08668a06e8fe0c7791ccae40
Olga <olga.filiushkina@gmail.com>
2020-04-14 20:06:16 -0400

    fixed hamburger's position; fixed page navigation

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 $(function() {
+    // menu opens when user clicks on hamburger menu 
     $('#hamburger').on('click', () => {
       $('.nav ul').slideDown();
     })
 
+    // menu closes when user clicks on close button
     $('#close').on('click', () => {
       $('.nav ul').slideUp();
     })
 
-    $('.nav-link').on('click', () => {
-      $('.nav ul').slideUp();
-    })
-    
+    // on mobile and small tablet the menu closes when user clicks on a button
+    if (screen.width < 768 ) {
+      $('.nav-link').on('click', () => {
+        $('.nav ul').slideUp();
+      })
+    }
 });
